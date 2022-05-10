f8caf65c517aa7eebd571a74f4ba25c22141face
Zhaulanbekk <zhaulanbek00>
2022-05-10 13:55:21 +0600

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int[] aty = new int[300];
+        System.out.println(method(300));
+    }
+     static int method(int aty){
+       // int[] aty = new int[300];
         int count=0;
         int sum=0;
-        for (int i = 1; i < aty.length; i++) {
+        for (int i = 1; i < aty; i++) {
             if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0){
                 System.out.print(i+" ");
                 sum+=i;
@@ -17,6 +20,7 @@ public class Main {
         }
         System.out.println();
         System.out.println(count+": san tak daana bolunot ");
-        System.out.println("obshiy summasy: "+sum);
-    }
+        System.out.print("obshiy summasy: ");
+        return sum;
+     }
 }
